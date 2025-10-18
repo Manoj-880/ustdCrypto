3218818a42b9b7d6ca78d561760b0bde638d53ec
Manoj-880 <manoj.inamanamelluri123@gmail.com>
2025-10-18 11:23:11 +0530

    cors update

--- usdtBackend/server.js
+++ usdtBackend/server.js
@@ -29,8 +29,9 @@ const corsOptions = {
       'http://localhost:5173',
       'http://127.0.0.1:3000',
       'http://127.0.0.1:5173',
-	'https://www.secureusdt.com',
-	'https://api.secureusdt.com'
+        'https://www.secureusdt.com',
+        'https://api.secureusdt.com',
+        'https://secureusdt.com'
     ];
 
     if (allowedOrigins.indexOf(origin) !== -1) {
